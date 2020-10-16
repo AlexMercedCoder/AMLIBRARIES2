3b7cd9ceb5cf2c33907d88fbdb74d014b7b53599
Alex Merced <willcodeforfood@alexmerced.com>
2020-10-16 13:48:04 -0400

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({ proj }) => {
+  return <article>
+      <h2>{proj.name}</h2>
+      <p>{proj.description}</p>
+      <a href={proj.git}><h5>Github</h5></a>
+      <a href={proj.npm}><h5>NPM</h5></a>
+      <a href={proj.vid}><h5>Video Tut</h5></a>
+      <a href={proj.blog}><h5>Blog Tut</h5></a>
+      <code>{proj.command}</code>
+  </article>;
+};
+
+export default Card;
---
+++ src/lib.js
@@ -0,0 +1,186 @@
+export default [
+  {
+    name: "MercedUI",
+    description:
+      "This is my main Front-End UI library built around the Web Component API with tools to create reactive components, local and global state management, tools for forms, capturing element props and url queries, and component routing.",
+    github: "https://github.com/AlexMercedCoder/MercedUI",
+    npm: "https://www.npmjs.com/package/mercedui",
+    vid:
+      "https://www.youtube.com/playlist?list=PLY6oTPmKnKbYrP3DfCUTYYADu0IT9DRZZ",
+    blog: "https://tuts.alexmercedcoder.com/mercedui/",
+    command: "npx create-mercedui-app projectName",
+  },
+  {
+    name: "MercedElement",
+    description:
+      "MercedElement is a class to create WebComponents which is at the core of the MercedUI library, this library only includes MercedElement for those who don't need the other bells and whistles.",
+    github: "https://github.com/AlexMercedCoder/MercedElement",
+    npm: "https://www.npmjs.com/package/mercedelement",
+    vid: "https://www.youtube.com/watch?v=p0pT8egjpR4",
+    blog: "",
+    command: "npm install mercedelement",
+  },
+  {
+    name: "ChainElement",
+    description:
+      "ChainElement is a web component class that creates reactive components but also passes url queries and global data to its components at render. This is a standalone library, this class IS NOT part of the MercedUI library.",
+    github: "https://github.com/AlexMercedCoder/ChainElement",
+    npm: "https://www.npmjs.com/package/chainelement",
+    vid: "https://www.youtube.com/watch?v=9uP17YUjpJA",
+    blog: "",
+    command: "npm install chainelement",
+  },
+  {
+    name: "MercedUtils",
+    description:
+      "MercedUtils is a library that add several useful tools and functions to the array prototype. If you prefer these functions not be added to the array prototype use the Bettertypes or mUtils library as an alternative.",
+    github: "https://github.com/AlexMercedCoder/JS_Utilities",
+    npm: "https://www.npmjs.com/package/mercedutils",
+    vid:
+      "https://www.youtube.com/playlist?list=PLY6oTPmKnKbbge3XP8ne041HAQwGle8_Z",
+    blog: "",
+    command: "npm install mercedutils",
+  },
+  {
+    name: "BetterTypes",
+    description:
+      "BetterTypes offers many of the same utilities that MercedUtils does but does through new classes vs adding functions to the array and string prototypes.",
+    github: "https://github.com/AlexMercedCoder/bettertypesjs",
+    npm: "https://www.npmjs.com/package/@alexmerced/bettertypes",
+    vid:
+      "https://www.youtube.com/playlist?list=PLY6oTPmKnKbbge3XP8ne041HAQwGle8_Z",
+    blog: "",
+    command: "npm install @alexmerced/bettertypes",
+  },
+  {
+    name: "Mutils",
+    description:
+      "A Utility library of utility classes that extend Sets, Arrays and help work with Random Numbers.",
+    github: "https://github.com/AlexMercedCoder/Mutils",
+    npm: "https://www.npmjs.com/package/mutils_am",
+    vid: "https://www.youtube.com/watch?v=Fn8-AFZ9Lsw",
+    blog: "",
+    command: "npm install mutils_am",
+  },
+  {
+    name: "BasicElement",
+    description:
+      "BasicElement aims to be the simplest possible extension to HTMLElement adding react like state and props and nothing else...",
+    github: "https://github.com/AlexMercedCoder/BasicElement",
+    npm: "https://github.com/AlexMercedCoder/BasicElement",
+    vid: "https://www.youtube.com/watch?v=QsmMGaPPm7g",
+    blog: "",
+    command: "npx merced-spinup basicelement projectName",
+  },
+  {
+    name: "ComponentZoo",
+    description:
+      "ComponentZoo aims to house all the HTMLElement extended classes from all my other libraries and more including a Router Component, StyleWrapper Component (think styled components), and FormWrapper (FormTool as a Component).",
+    github: "https://github.com/AlexMercedCoder/ComponentZoo",
+    npm: "https://www.npmjs.com/package/componentzoo",
+    vid: "https://www.youtube.com/watch?v=EE0N7Yluu6o",
+    blog: "",
+    command: "npx merced-spinup componentzoo projectName",
+  },
+  {
+    name: "FunComponent",
+    description:
+      "Allows you to make web component in a functional style that enables the creation of hooks.",
+    github: "https://github.com/AlexMercedCoder/funComponent",
+    npm: "https://www.npmjs.com/package/funcomponents",
+    vid: "https://www.youtube.com/watch?v=5_EIaA9iyPw",
+    blog: "",
+    command: "npx create-funcomponent-app projectName",
+  },
+  {
+    name: "mBlocks",
+    description:
+      "A class based front-end UI library for creating components without the web-component api or other tooling like Babel or JSX.",
+    github: "https://github.com/AlexMercedCoder/mBlocks",
+    npm: "https://www.npmjs.com/package/mblocks",
+    vid: "https://www.youtube.com/watch?v=MglHxXiSMwA",
+    blog: "https://tuts.alexmercedcoder.com/mblocks/",
+    command: "npx merced-spinup mblocks projectName",
+  },
+  {
+    name: "superFunc UI",
+    description:
+      "A function based front-end UI library for creating components without the web-component api or other tooling like Babel or JSX.",
+    github: "https://github.com/AlexMercedCoder/superFunc",
+    npm: "https://www.npmjs.com/package/superfuncui",
+    vid: "https://www.youtube.com/watch?v=pZRD1Iq99Hs",
+    blog: "",
+    command: "npx merced-spinup superfunc projectName",
+  },
+  {
+    name: "Merced-Reactor",
+    description:
+      "A tool for quickly generating lightweight react projects, think of it as a slimmer create-react-app.",
+    github: "https://github.com/AlexMercedCoder/merced-reactor",
+    npm: "https://www.npmjs.com/package/merced-reactor",
+    vid: "https://www.youtube.com/watch?v=HtjvYdlYJ38",
+    blog: "",
+    command:
+      "'npx merced-reactor projectName js' or 'npx merced-reactor projectName ts'",
+  },
+  {
+    name: "Merced-Vuer",
+    description:
+      "A tool for generating a Vue project with Router and the Buefy component library already configured.",
+    github: "https://github.com/AlexMercedCoder/merced-vuer",
+    npm: "https://www.npmjs.com/package/merced-vuer",
+    vid: "https://www.youtube.com/watch?v=gmOOGs59KN4",
+    blog: "",
+    command: "npx merced-vuer projectName",
+  },
+  {
+    name: "RenderBlocks",
+    description:
+      "A library for having basic reactive UI features in a small package.",
+    github: "https://github.com/AlexMercedCoder/RenderBlocks",
+    npm: "https://www.npmjs.com/package/renderblocks",
+    vid: "https://www.youtube.com/watch?v=xskOxRo3tqc",
+    blog: "https://tuts.alexmercedcoder.com/renderblocks/",
+    command: "npx merced-spinup renderblocks projectName",
+  },
+  {
+    name: "AMPonent",
+    description:
+      "A Web Component Library for building reactive and styling web components.",
+    github: "https://github.com/AlexMercedCoder/Ponent",
+    npm: "https://www.npmjs.com/package/amponent",
+    vid: "https://www.youtube.com/watch?v=Gy0SeuYIY8o",
+    blog: "https://tuts.alexmercedcoder.com/amponenttut/",
+    command: "npx create-amponent-app projectName",
+  },
+  {
+    name: "Merced-Spinup",
+    description:
+      "A tool for spinning up a projects in several different backend and frontend frameworks, read the docs to see the full list of templates.",
+    github: "https://github.com/AlexMercedCoder/merced-spinup",
+    npm: "https://www.npmjs.com/package/merced-spinup",
+    vid: "https://www.youtube.com/watch?v=7msl1qJsA1U",
+    blog: "https://tuts.alexmercedcoder.com/mercedspinuptut/",
+    command: "npx merced-spinup templateName projectName",
+  },
+  {
+    name: "Merver",
+    description:
+      "A MicroWebserver framework for building APIs and Microservices.",
+    github: "https://github.com/AlexMercedCoder/merver",
+    npm: "https://www.npmjs.com/package/am-merver",
+    vid: "https://youtu.be/kejYk7KsH78",
+    blog: "",
+    command: "npx merced-spinup merver projectName",
+  },
+  {
+    name: "useDataStore",
+    description:
+      "A mini React library for scaffolding Application level state in React.",
+    github: "https://github.com/AlexMercedCoder/useDataStore",
+    npm: "https://www.npmjs.com/package/usedatastore",
+    vid: "https://www.youtube.com/watch?v=62tzQ8cFxpM",
+    blog: "https://tuts.alexmercedcoder.com/usedatastore/",
+    command: "npm install usedatastore",
+  },
+];
