882ad2afe8ac4d93245ee33ccf44438d8bc5113e
Alex Merced <willcodeforfood@alexmerced.com>
2020-10-19 21:30:34 -0400

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 const Card = ({ proj }) => {
-  return <article>
+  return (
+    <article>
       <h2>{proj.name}</h2>
       <p>{proj.description}</p>
-      <a href={proj.git}><h5>Github</h5></a>
-      <a href={proj.npm}><h5>NPM</h5></a>
-      <a href={proj.vid}><h5>Video Tut</h5></a>
-      <a href={proj.blog}><h5>Blog Tut</h5></a>
+      <a href={proj.github}>
+        <h5>Github</h5>
+      </a>
+      <a href={proj.npm}>
+        <h5>NPM</h5>
+      </a>
+      <a href={proj.vid}>
+        <h5>Video Tut</h5>
+      </a>
+      <a href={proj.blog}>
+        <h5>Blog Tut</h5>
+      </a>
       <code>{proj.command}</code>
-  </article>;
+    </article>
+  );
 };
 
 export default Card;
